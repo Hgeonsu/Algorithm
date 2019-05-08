b7247ea9d2c3048ee9040e3786f5ed487240e795
Hgeonsu <37791388+Hgeonsu@users.noreply.github.com>
2019-05-08 23:27:26 +0900

    review@@@@@@@@

--- boj_7569_토마토_bfs.cpp
+++ boj_7569_토마토_bfs.cpp
@@ -1,108 +1,78 @@
 #include<iostream>
 #include<queue>
 using namespace std;
-int dh[6] = { 0,0,0,0,-1,1 }; //동서남북위아래
-int dr[6] = { 0,0,1,-1,0,0 };
+int H, N, M; //N = row, N = col
+int map[101][101][101], vis[101][101][101];
+int dh[6] = { 0,0,0,0,1,-1 };
 int dc[6] = { 1,-1,0,0,0,0 };
-queue<pair<int, pair<int, int> > > q;
-int map[101][101][101];
-int vis[101][101][101];
-int M, N, H;
+int dr[6] = { 0,0,1,-1,0,0 };
 int day = 0;
-
-bool bfs() {
-	//시작점 큐에 넣고 방문처리 하는 거 필요 없음
-	bool changed = false;
-
+queue<pair<int, pair<int, int> > > q;
+bool updated = false;
+void bfs() {
+	
 	while (!q.empty()) {
+		int size = q.size();//하루 단위로 bfs실행하기위해@@@@
+		for (int i = 0; i < size; i++) {
 		pair<int, pair<int, int> > cur = q.front();
 		q.pop();
+		
+		// 큐에 있는만큼만 실행해야 하루 단위로 계산 가능
+		//cout << q.size() << '
+		
+			for (int j = 0; j < 6; j++) {
+				int nh = cur.first + dh[j];
+				int nr = cur.second.first + dr[j];
+				int nc = cur.second.second + dc[j];
+				
+				//박스 밖으로 나가는 경우 continue
+				if (nh < 0 || nr < 0 || nc < 0 || nh >= H || nr >= N || nc >= M)continue;
+				//빈 박스인 경우 continue
+				if (map[nh][nr][nc] == -1 || vis[nh][nr][nc] ==1) continue;
+				map[nh][nr][nc] = 1;
+				updated = true;
+				q.push({ nh,{nr, nc} });
+				vis[nh][nr][nc] = 1;
 
-		for (int i = 0; i < 6; i++) {
-			int nh = cur.first + dh[i];
-			int nr = cur.second.first + dr[i];
-			int nc = cur.second.second + dc[i];
-
-			if (nh < 0 || nr < 0 || nc < 0 || nh >= H || nr >= N || nc >= M)continue;
-			if (vis[nh][nr][nc] == 1 || map[nh][nr][nc] == -1)continue; //토마토가 없는 위치거나, 이미 확산을 한 토마토면 continue
-
-			//q.push({ nh,{nr, nc} });
-			//vis[nh][nr][nc] = 1;
-			map[nh][nr][nc] = 1;
-			changed = true;
+			}
+		}
+		if (updated) {
+			day++;
+			updated = false;
+		}
+		else {
+			return;
 		}
 	}
-	return changed;
 }
-
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+	
 	cin >> M >> N >> H;
-	// N이 row, M이 col
+
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < N; j++) {
 			for (int k = 0; k < M; k++) {
 				cin >> map[i][j][k];
-			}
-		}
-	}
-	bool again = true;
-	
-	while (1) {
-		
-
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < M; k++) {
-					//토마토가 있고, 이전에 확산한적이 없는 곳이면
-					if (map[i][j][k] == 1 && vis[i][j][k] == 0) {
-						q.push({ i, {j, k} });
-						//큐에 들어갔으면 다시 들어갈 필요가 없음
-						vis[i][j][k] = 1;
-					}
+				if (map[i][j][k] == 1) {
+					q.push({ i,{j,k} });
+					vis[i][j][k] = 1;
 				}
 			}
 		}
-		again = bfs();
-		/*cout << "again :  " << again << "\nday : " << day<<'\n';
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < M; k++) {
-					cout << vis[i][j][k];
-				}
-				cout << '\n';
-			}
-			cout << '\n';
-		}*/
-		if (again == false)break;
-		
-		day++;
 	}
 
-	
-	//cout << "\n결과 map\n";
-	//cout << "day = " << day << '\n';
-	//for (int i = 0; i < H; i++) {
-	//	for (int j = 0; j < N; j++) {
-	//		for (int k = 0; k < M; k++) {
-	//			cout << map[i][j][k];
-	//		}
-	//		cout << '\n';
-	//	}
-	//	cout << '\n';
-	//}
+	bfs();
 
-	//int result;
+	//익지 않은 토마토가 있으면
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < N; j++) {
 			for (int k = 0; k < M; k++) {
-				if (map[i][j][k] == 0)
+				if (map[i][j][k] == 0) {
 					day = -1;
+				}
 			}
 		}
 	}
-
 	cout << day << '\n';
 	return 0;
 }
